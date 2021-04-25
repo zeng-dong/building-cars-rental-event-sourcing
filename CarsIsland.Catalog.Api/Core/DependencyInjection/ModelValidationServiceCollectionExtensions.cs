b5bbc32f3cd0ee263019be54bd3c34c372ae40ea
Dong Zeng <dong@freshbytesoftware.com>
2021-04-25 10:08:19 -0500

    created Catalog.Api, added model validation

---
+++ CarsIsland.Catalog.Api/Core/DependencyInjection/ModelValidationServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CarsIsland.Catalog.Api.Infrastructure.Validators;
+using CarsIsland.Catalog.Domain.Model;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarsIsland.Catalog.Api.Core.DependencyInjection
+{
+    public static class ModelValidationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddModelValidators(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<Car>, CarValidator>();
+            return services;
+        }
+    }
+}
