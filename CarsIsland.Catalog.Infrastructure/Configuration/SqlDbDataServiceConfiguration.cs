f49aa176cc44c55e96fa6af9fb48438e2758b9f5
Dong Zeng <dong@freshbytesoftware.com>
2021-04-25 09:55:38 -0500

    added Catalog.Infrastructure

---
+++ CarsIsland.Catalog.Infrastructure/Configuration/SqlDbDataServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using CarsIsland.Catalog.Infrastructure.Configuration.Interfaces;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace CarsIsland.Catalog.Infrastructure.Configuration
+{
+    public class SqlDbDataServiceConfiguration : ISqlDbDataServiceConfiguration
+    {
+        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    public class SqlDbDataServiceConfigurationValidation : IValidateOptions<SqlDbDataServiceConfiguration>
+    {
+        public ValidateOptionsResult Validate(string name, SqlDbDataServiceConfiguration options)
+        {
+            if (string.IsNullOrEmpty(options.ConnectionString))
+            {
+                return ValidateOptionsResult.Fail($"{nameof(options.ConnectionString)} configuration parameter for the Azure SQL is required");
+            }
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
