4a14539abc088b88a8e7e90ff0931e1c66d02bc9
Dong Zeng <dong@freshbytesoftware.com>
2021-05-05 08:49:41 -0500

    defined IEventLogService, added EventLogService to implement the interface

---
+++ CarsIsland.EventLog/Services/EventLogService.cs
@@ -0,0 +1,38 @@
+﻿using CarsIsland.EventBus.Events;
+using CarsIsland.EventLog.Entries;
+using CarsIsland.EventLog.Services.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CarsIsland.EventLog.Services
+{
+    public class EventLogService : IEventLogService
+    {
+        public Task MarkEventAsFailedAsync(Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MarkEventAsInProgressAsync(Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MarkEventAsPublishedAsync(Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CarsIsland.EventLog/Services/Interfaces/IEventLogService.cs
+++ CarsIsland.EventLog/Services/Interfaces/IEventLogService.cs
@@ -1,6 +1,17 @@
-﻿namespace CarsIsland.EventLog.Services.Interfaces
+﻿using CarsIsland.EventBus.Events;
+using CarsIsland.EventLog.Entries;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CarsIsland.EventLog.Services.Interfaces
 {
     public interface IEventLogService
     {
+        Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId);
+        Task SaveEventAsync(IntegrationEvent @event, Microsoft.EntityFrameworkCore.Storage.IDbContextTransaction transaction);
+        Task MarkEventAsPublishedAsync(Guid eventId);
+        Task MarkEventAsInProgressAsync(Guid eventId);
+        Task MarkEventAsFailedAsync(Guid eventId);
     }
 }
